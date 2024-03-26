003aa4ff12c1bb22e4b6ada75ce9f539139e0521
Lee Morgan <me@leemorgan.io>
2024-03-26 15:53:22 -0400

    Send 'get office' websocket message from office page.

--- views/coworking/js/pages/office.js
+++ views/coworking/js/pages/office.js
@@ -1,5 +1,21 @@
 module.exports = {
+    rendered: false,
+
     render: function(officeId){
-        console.log("rendering office");
+        if(!rendered){
+            this.getOffice(officeId);
+
+            this.rendered = true;
+        }
+    },
+
+    getOffice: function(id){
+        let data = {
+            action: "getOffice",
+            user: localStorage.getItem("coworkToken"),
+            office: id
+        };
+
+        socket.send(data);
     }
 }
